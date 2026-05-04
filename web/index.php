e580fc74d92cf03a930a0561d5cba28b75829ece
sadeemkn <2005sadeem20@gmail.com>
2026-05-04 09:44:13 +0300

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Simple Demo</title>
+  </head>
+  <body>
+    <h1>Welcome to My Demo</h1>
+    <p>This is a very simple project uploaded to GitHub.</p>
+  </body>
+</html>
